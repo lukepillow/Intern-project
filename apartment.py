c5d75aa76d6ad5fcb737cb1374fe17dd6d19dc0d
GarrettConway <gconway@g.hmc.edu>
2017-07-25 10:30:14 -0700

    Add files via upload

--- apartment.py
+++ apartment.py
@@ -3,7 +3,7 @@ import json
 from bs4 import BeautifulSoup
 
 
-headers = {'accept' : 'application/json, text/javascript, */*; q=0.01',
+infoCardHeaders = {'accept' : 'application/json, text/javascript, */*; q=0.01',
 'accept-encoding':'gzip, deflate, br',
 'accept-language':'en-US,en;q=0.8',
 'content-type':'application/json',
@@ -12,10 +12,23 @@ headers = {'accept' : 'application/json, text/javascript, */*; q=0.01',
 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
 'x-requested-with':'XMLHttpRequest'}
 
-url = 'https://www.apartments.com/services/property/infoCardData'
-
 def getInfo(listingID):
 	'''Takes a string, listingID, and returns the infoCardData for that ID.'''
 	payload = {"ListingKeys":[listingID],"SearchCriteria":{}}
-	req = requests.post(url, headers=headers, json=payload)
-	return req.json()
+	req = requests.post('https://www.apartments.com/services/property/infoCardData', headers=infoCardHeaders, json=payload)
+	#return req.json()
+	return req
+
+	
+def parseIDs(idString):
+	#list is itemized in id | ? | listingIDlist for available ones with multiple, null for unavailable | lat | long
+	list = idString.split('|')
+
+
+
+def parseInfo(infoJSON):
+	name = infoJSON['PropertyName']
+	url = infoJSON['PropertyUrl']
+	rating = infoJSON['Listing']['Rating']
+	latitude = infoJSON['Listing']['Address']['Location']['Latitude']
+	longitude = infoJSON['Listing']['Address']['Location']['Longitude']
