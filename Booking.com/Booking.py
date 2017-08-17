78cf217a5f5d0c4c0c2aea50c5ed6ef7bb5c069d
GarrettConway <gconway@g.hmc.edu>
2017-08-16 18:10:02 -0700

    Review api added

--- Booking.com/Booking.py
+++ Booking.com/Booking.py
@@ -18,7 +18,7 @@ def getFeatures(url):
 	#
 	description = soup.find(class_='hp_desc_main_content').text.strip()
 	description_marker = -(description[::-1].find("What would you like to know?"[::-1])+len("What would you like to know?"))
-	description = description[:description_marker].strip()									# Cuts off the extra added at the tail
+	description = description[:description_marker].strip()								# Cuts off the extra added at the tail
 	
 	#
 	facilities_list = []
@@ -31,16 +31,16 @@ def getFeatures(url):
 	
 	
 	# Extracts the first javascript header, which is parsable with demjson
-	#first_json = demjson.decode(soup.find_all('script')[1])
+	first_json = demjson.decode(soup.find_all('script')[1].text)
 	#
-	#map = first_json['hasMap']
-	#type = first_json['@type']
-	#name = first_json['name']
-	#description_mini = first_json['description']
-	#priceRange_text = first_json['priceRange']
-	#url_again = first_json['url']
-	#address_json = first_json['address']
-	#rating_json = first_json['aggregateRating']
+	map = first_json['hasMap']
+	type = first_json['@type']
+	name = first_json['name']
+	description_mini = first_json['description']
+	priceRange_text = first_json['priceRange']
+	url_again = first_json['url']
+	address_json = first_json['address']
+	rating_json = first_json['aggregateRating']
 	
 	# TODO
 	#second_json = soup.find_all('script')[2]
@@ -59,18 +59,41 @@ def getFeatures(url):
 		review_sub_score_breakdown.append('('+sub_score_name+'): ' + sub_score_value)
 	
 	
-	recent_reviews = []
-	for review in soup.find(class_="review_list hp_recent_property_reviews_container").find_all(class_="review_item clearfix "):
-		review_date = review.find(class_="review_item_date").text.strip()
-		review_name = review.find(class_="review_item_reviewer").h4.text.strip()
-		
-		review_title = review.find(class_="review_item_header_content recent_property_review_title").text.strip()
-		review_score = review.find(class_="review-score-badge").text.strip()
-		review_tags = [tag.text.strip()[2:] for tag in review.find(class_="review_item_info_tags").find_all(class_="review_info_tag")]
-		#^Removes the bullet points
-		review_text_negative = review.find(class_="review_neg").text.strip()[1:]
-		review_text_positive = review.find(class_="review_pos").text.strip()[1:]
+	#recent_reviews = []
+	#for review in soup.find(class_="review_list hp_recent_property_reviews_container").find_all(class_="review_item clearfix "):
+	#	review_date = review.find(class_="review_item_date").text.strip()
+	#	review_name = review.find(class_="review_item_reviewer").h4.text.strip()
+	#	
+	#	review_title = review.find(class_="review_item_header_content recent_property_review_title").text.strip()
+	#	review_score = review.find(class_="review-score-badge").text.strip()
+	#	#^Removes the bullet points
+	#	review_text_negative = review.find(class_="review_neg").text.strip()[1:]
+	#	review_text_positive = review.find(class_="review_pos").text.strip()[1:]
 			
 	return (title, description, facilities_list, review_score_breakdown, review_sub_score_breakdown)
+
+
+def getReviewsRequest(url, offset=0):
+	'''Returns a request object for the first 100 reviews after the offset.'''
+	# Uses the booking.com reviewlist api
+	headers={'Cookie':'lastSeen=1502925988969',
+	'Host':'www.booking.com',
+	'Upgrade-Insecure-Requests':'1',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'}
+	parameters = {'cc1':'us', 'dist':1, 'pagename': None, 'r_lang':'en', 'type':'total', 'offset':offset, 'rows':100}
+	newUrl = 'https://www.booking.com/reviewlist.en-gb.html'
 	
+	# For the request, you must set 'cc1' to the country code and 'pagename' to the hotel page's name
+	parameters['pagename'] = getPagename(url)
+	parameters['cc1'] = getCountryCode(url)
 	
+	return requests.get(newUrl, data=parameters, headers=headers)
+	
+
+def getPagename(url):
+	return url[-url[::-1].find('/'):-(url[::-1].find('.en'[::-1])+3)]
+
+def getCountryCode(url):
+	# Assumes country code is always 2 letters
+	start = -(url[::-1].find('/')+3)
+	return url[start:start+2]
